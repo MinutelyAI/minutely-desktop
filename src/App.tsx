a393c0a88f04c9f34c19c93b7aac13e67732a77b
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2026-03-10 20:57:23 +0500

    feat: compete the sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import ProtectedRoute from "@/components/protected-routes";
 
 export default function App() {
   const isLoggedIn = localStorage.getItem("auth") === "true";
+  const defaultDashboardRoute = "/dashboard/meetings"
 
   return (
     <Routes>
       <Route
         path="/"
-        element={<Navigate to={isLoggedIn ? "/dashboard" : "/login"} replace />}
+        element={<Navigate to={isLoggedIn ? defaultDashboardRoute : "/login"} replace />}
       />
-      <Route path="/login" element={isLoggedIn ? <Navigate to="/dashboard" replace /> : <LoginPage />} />
+      <Route
+        path="/login"
+        element={isLoggedIn ? <Navigate to={defaultDashboardRoute} replace /> : <LoginPage />}
+      />
       <Route path="/dashboard" element={
+        <Navigate to={defaultDashboardRoute} replace />
+      } />
+      <Route
+        path="/dashboard/:sectionId"
+        element={
           <ProtectedRoute>
             <Dashboard />
           </ProtectedRoute>
