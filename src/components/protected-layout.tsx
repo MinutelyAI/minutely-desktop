0262947283be380f654ce4de325473d253922a41
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2026-03-20 14:40:25 +0500

    feat: added notification icon

--- src/components/protected-layout.tsx
+++ src/components/protected-layout.tsx
@@ -1,12 +1,17 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import { Link, Outlet, useLocation } from "react-router-dom"
 
 import { AppSidebar as Sidebar } from "@/components/sidebar"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator, } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+// import { Bell as Notification, BellDot as NewNotification } from "lucide-react"
+import { BellIcon as Notif } from '@heroicons/react/24/outline'
+import { BellIcon as NotifOpen } from '@heroicons/react/24/solid'
 
 export default function ProtectedLayout() {
+  const [notifOpen, setNotifOpen] = useState(false)
+
   const { pathname } = useLocation()
   const breadcrumbs = (() => {
     switch (pathname) {
@@ -38,37 +43,47 @@ export default function ProtectedLayout() {
     <SidebarProvider>
       <Sidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            {breadcrumbs.length > 0 && (
-              <Breadcrumb>
-                <BreadcrumbList>
-                  {breadcrumbs.map((crumb, index) => {
-                    const isLast = index === breadcrumbs.length - 1
+        <header className="flex h-16 shrink-0 items-center px-4">
+          <div className="flex w-full items-center justify-between">
 
-                    return (
-                      <Fragment key={`${crumb.label}-${index}`}>
-                        <BreadcrumbItem>
-                          {isLast || !crumb.to ? (
-                            <BreadcrumbPage>{crumb.label}</BreadcrumbPage>
-                          ) : (
-                            <BreadcrumbLink render={<Link to={crumb.to} />}>
-                              {crumb.label}
-                            </BreadcrumbLink>
+            <div className="flex items-center gap-2">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" />
+
+              {breadcrumbs.length > 0 && (
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    {breadcrumbs.map((crumb, index) => {
+                      const isLast = index === breadcrumbs.length - 1
+
+                      return (
+                        <Fragment key={`${crumb.label}-${index}`}>
+                          <BreadcrumbItem>
+                            {isLast || !crumb.to ? (
+                              <BreadcrumbPage>{crumb.label}</BreadcrumbPage>
+                            ) : (
+                              <BreadcrumbLink render={<Link to={crumb.to} />}>
+                                {crumb.label}
+                              </BreadcrumbLink>
+                            )}
+                          </BreadcrumbItem>
+                          {!isLast && (
+                            <BreadcrumbSeparator className="hidden md:block" />
                           )}
-                        </BreadcrumbItem>
-                        {!isLast && <BreadcrumbSeparator className="hidden md:block" />}
-                      </Fragment>
-                    )
-                  })}
-                </BreadcrumbList>
-              </Breadcrumb>
+                        </Fragment>
+                      )
+                    })}
+                  </BreadcrumbList>
+                </Breadcrumb>
+              )}
+            </div>
+
+            {notifOpen ? (
+              <NotifOpen onClick={() => setNotifOpen(false)} className="w-5 h-5 cursor-pointer" />
+            ) : (
+              <Notif onClick={() => setNotifOpen(true)} className="w-5 h-5 cursor-pointer" />
             )}
+
           </div>
         </header>
         <Outlet />
